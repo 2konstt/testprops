037fc8ee99affa75277c742b08b624c88e6ec9c7
2konstt <45565873+2konstt@users.noreply.github.com>
2019-07-30 20:26:56 +0700

    1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,113 @@
 import React from 'react';
-import logo from './logo.svg';
+import PropTypes from 'prop-types';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      count: 5,
+      level: 0,
+      num: 0,
+      gameStart: false,
+      timerON: false
+    }
+  }
+
+  getRandomMath = () => {
+    var newNum = Math.floor(Math.random() * ((Math.pow(10, this.state.level+1)-1)-Math.pow(10, this.state.level))) + Math.pow(10, this.state.level)
+    this.setState({
+      num: newNum,
+      gameStart: true,
+      timerON: true
+    })
+
+    var timer = setInterval(() => {
+      var count1 = this.state.count;
+      if(count1===0){
+        clearInterval(timer)
+        this.setState({
+          count: 5,
+          timerON: false
+        })
+  
+      } else {
+        this.setState({
+        count: count1-1,
+        timerON: true
+        })
+      }
+    },1000)
+
+
+  }
+
+    
+   
+  
+  
+ render() {
+    return (
+      <div>
+        {
+          !this.state.gameStart ? <button onClick={this.getRandomMath}>START THE GAME</button> :
+        
+        
+          <Card 
+            num={this.state.num}
+            level={this.state.level} 
+            func={()=>{
+              if (this.state.num == this.state.input) {
+                var lvlUp = this.state.level + 1
+                this.setState({
+                            level: lvlUp, 
+                            input: ''
+                            })
+                this.getRandomMath()
+              } else {
+                this.setState({level: 0, gameStart: false})
+              }
+            }}
+            timerON={this.state.timerON}
+            count={this.state.count}
+            changefunc={(e)=>{
+              this.setState({
+                input: e.target.value
+              })
+            }}
+          />
+        }
+      </div>
+    )
+  }
+}
+
+function Card(props){
+  return <div>
+          <h1>ЗАПОМНИ ЧИСЛО!</h1>
+          {
+            !props.timerON ? <p> </p> : <h3> {props.count} секунд</h3>
+          }
+          {
+            props.timerON ? <h1>{props.num}</h1> : <input onChange={props.changefunc} />
+          }
+          {
+            props.timerON ? <p> </p> : <button onClick={props.func}>CLICK</button>
+          }
+          
+          <p>LEVEL: {props.level}</p>
+        </div>
+}
+
+Card.defaultProps = {
+  num: 'Все пошло по пизде'
+}
+
+Card.propTypes = {
+  num: PropTypes.number,
+  level: PropTypes.number,
+  func: PropTypes.func.isRequired,
+  changefunc: PropTypes.func.isRequired
 }
 
 export default App;
